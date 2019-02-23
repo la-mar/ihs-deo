e902aa2b1171a79731b4bb66ed63b5a73ac17526
la-mar <brocklfriedrich@gmail.com>
2019-02-23 10:14:17 -0600

    YEAH

--- app/brock.py
+++ app/brock.py
@@ -11,11 +11,18 @@ QB = zeep.Client(wsdl=qb_wsdl)
 EB = zeep.Client(wsdl=eb_wsdl)
 # client = zeep.Client(wsdl=wsdl)
 
+domain = 'US'
+
+dtypes = ['Well', 'Production Allocated']
+
 def methods(obj) -> None:
     return [x for x in dir(obj) if not x.startswith('__')]
 
 def m(obj):
     return methods(obj)
+
+def get_count():
+    'Put query that worked here'
 
 from zeep import xsd
 
@@ -44,12 +51,33 @@ _soapheaders = [header_value]
 
 CC.service.Login(_soapheaders = _soapheaders)
 
-s.GetEntitlements(_soapheaders = _soapheaders) # works
+CC.service.GetEntitlements(_soapheaders = _soapheaders) # works
 
-
-c = EB
 s = EB.service
 
+EB.service.GetExportTemplates(domain, 'Well', _soapheaders)
 
+wellquery = """
+<criterias>
+  <criteria type="value">
+    <domain>US</domain>
+    <datatype>Production Allocated</datatype>
+    <attribute_group>Location</attribute_group>
+    <attribute>Basin</attribute>
+    <type>name</type>
+    <filter logic="equals">
+      <value  actual="PERMIAN BASIN"/>
+    </filter>
+  </criteria>
+</criterias>
+"""
 
+s = QB.service
 
+s.GetCount(wellquery, 'Well', 'US', _soapheaders = _soapheaders)
+
+s.LookupCode('UWI', )
+
+                         LookupCode(Parameters: ns2: LookupParameters, _soapheaders = {
+                              request_header: ns0: Header
+                         })
