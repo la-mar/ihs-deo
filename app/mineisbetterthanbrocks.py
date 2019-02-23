65ecb4cf5d3686eaa9dfde23978fa1fc1f6752a7
Brock Friedrich <brocklfriedrich@gmail.com>
2019-02-23 14:01:02 -0600

    committing changes

--- app/mineisbetterthanbrocks.py
+++ app/mineisbetterthanbrocks.py
@@ -37,19 +37,46 @@ header_value = header(Username=user, Password = password, Application = appName)
 
 client.service.Login(_soapheaders=[header_value])
 
-qb_client = zeep.Client(wsdl = qb_wsdl)
+# qb_client = zeep.Client(wsdl = qb_wsdl)
 
-qb_query = """<criterias>
-  <criteria type="value">
-    <domain>US</domain>
-    <datatype>Well</datatype>
-    <attribute_group>Location</attribute_group>
-    <attribute>Basin</attribute>
-    <type>name</type>
-    <filter logic="equals">
-      <value  actual="PERMIAN BASIN"/>
-    </filter>
-  </criteria>
-</criterias>"""
+# qb_query = """<criterias>
+#     <criteria type="group" groupId="" ignored="false">
+#         <domain>US</domain>
+#         <datatype>Well</datatype>
+#         <attribute_group>Identification</attribute_group>
+#         <attribute>Current Operator</attribute>
+#         <filter logic="include">
+#             <value id="0" ignored="false">
+#                 <group_actual>
+#                     <operator logic="and">
+#                         <condition logic="equals">
+#                             <attribute>code</attribute>
+#                             <value_list>
+#                                 <value>278107</value>
+#                             </value_list>
+#                         </condition>
+#                     </operator>
+#                 </group_actual>
+#                 <group_display>name = DRIFTWOOD ENERGY OPERATING LLC</group_display>
+#             </value>
+#         </filter>
+#     </criteria>
+# </criterias>"""
 
-print(qb_client.service.GetCount(qb_query, "Well", "US", _soapheaders=[header_value]))
+#print(qb_client.service.GetCount(qb_query, "Well", "US", _soapheaders=[header_value]))
+
+eb_client = zeep.Client(eb_wsdl)
+
+params = {
+'Domain':'US',
+'DataType': 'Well',
+'Template': 'EnerdeqML Well',
+'Ids': ["42383402790000"]
+}
+
+target = {
+  'Filename':'Sample',
+'Overwrite': 'False'
+}
+
+job_id = eb_client.service.BuildExport(params, target, _soapheaders=[header_value])
