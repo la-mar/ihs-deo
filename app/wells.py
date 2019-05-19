4b11dc692b9af2a344f63f1d8ec282929ff7611f
Natalie Friedrich <brocklfriedrich@gmail.com>
2019-05-18 19:44:03 -0500

    updated to foreach on wellbores

--- app/wells.py
+++ app/wells.py
@@ -11,9 +11,6 @@ from lxml import etree, objectify
 
 QUERY_DIR = 'queries/'
 
-
-
-
 well_query_driftwood = """<criterias>
     <criteria type="group" groupId="" ignored="false">
         <domain>US</domain>
@@ -61,11 +58,8 @@ def get_default_target():
             'Overwrite': 'True'
             }
 
-
 def is_complete(job_id):
     return exportbuilder.service.IsComplete(job_id, _soapheaders=[header_value])
-
-
 
 def driftwood_wells(decode = False):
 
@@ -99,7 +93,6 @@ def get_wells(decode = False):
     else:
         return data
 
-
 def elevate_api(wellbore: dict) -> dict:
     """ Moves a well's identification number (api) to the top level of
         the dictionary."""
@@ -118,31 +111,6 @@ def elevate_api(wellbore: dict) -> dict:
 
     return wellbore
 
-
-import types
-def lowerValues(arg):
-    print(arg)
-    # Handle iterables
-    if isinstance(arg, list):
-        return [lowerValues(item) for item in arg]
-    elif isinstance(arg, str):
-        return arg.lower()
-    else:
-        return arg
-
-from collections import OrderedDict
-def renameKeysToLower(iterable):
-    iterable = dict(iterable).copy()
-    if type(iterable) is dict:
-        for key in iterable.keys():
-            iterable[key.lower()] = iterable.pop(key)
-            if type(iterable[key.lower()]) is dict or type(iterable[key.lower()]) is list:
-                iterable[key.lower()] = renameKeysToLower(iterable[key.lower()])
-    elif type(iterable) is list:
-        for item in iterable:
-            item = renameKeysToLower(item)
-    return dict(iterable)
-
 def tolower(d: dict):
     result = {}
     for key, value in d.items():
@@ -153,7 +121,6 @@ def tolower(d: dict):
         else:
             result[key.lower()] = value
     return result
-
 
 if __name__ == "__main__":
 
@@ -172,26 +139,27 @@ if __name__ == "__main__":
     xml = objectify.fromstring(wells_bin)
     root = xml.getroottree().getroot()
     wellbores = [child for child in root.getchildren() if child.tag == 'WELLBORE']
-    c = wellbores[0]
-    # wellbores = xmltodict.parse(c)
 
-    #convert child into dictionary
-    xmltojson = etree.tostring(c)
-    wellbore = xmltodict.parse(xmltojson)['WELLBORE']
+    number_of_wellbores = len(wellbores)
+    for i in range(number_of_wellbores):
+         #convert child into dictionary
+        xmltojson = etree.tostring(wellbores[i])
+        wellbore = xmltodict.parse(xmltojson)['WELLBORE']
+        lower_wellbore = tolower(elevate_api(wellbore))
+        #to_file(json.dumps(lower_wellbore, indent = 4), 'wellbore'+str(i)+'.json')
 
-    lower_wellbore = {lowerValues(k) : v for k,v in wellbore.items()}
+        # for key, value in ids.items():
+        #     wellbore.update({x['@TYPE']: x['#text']})
+        #     wellbore.move_to_end(x['@TYPE'], last=False)
 
-    # for key, value in ids.items():
-    #     wellbore.update({x['@TYPE']: x['#text']})
-    #     wellbore.move_to_end(x['@TYPE'], last=False)
+        #to_file(json.dumps(wellbore, indent = 4), 'wellbore.json')
+        # write to mongodb
+        #db.wells.insert(wellbore)
 
-    #to_file(json.dumps(wellbore, indent = 4), 'wellbore.json')
-    # write to mongodb
-    #db.wells.insert(wellbore)
+        #x = db.wells.find_one({'api14': '42383374130000'})
 
-    #x = db.wells.find_one({'api14': '42383374130000'})
 
-data = tolower(wellbore)
+
 
 
 
