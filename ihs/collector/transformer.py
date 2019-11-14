5ecce08ef8e6e104210517d9df25add377797ac3
Brock Friedrich <brock@driftwoodenergy.com>
2019-11-14 09:00:21 -0600

    extracted xmlparser class from transformer

--- ihs/collector/transformer.py
+++ ihs/collector/transformer.py
@@ -1,120 +1,93 @@
 from __future__ import annotations
 
+import hashlib
 import logging
-from typing import Callable, Dict, List, Union  # pylint: disable=unused-import
 from collections import OrderedDict
+from typing import Callable, Dict, List, Union  # pylint: disable=unused-import
 
-import xmltodict
 
 import util
-from collector.parser import Parser
-
-sp = util.StringProcessor()
 
 logger = logging.getLogger(__name__)
 
 
-class Transformer(object):
-    """ Transform an XML response into a normalized Python object"""
+class WellboreTransformer:
+    @classmethod
+    def extract_metadata(cls, data: OrderedDict) -> OrderedDict:
+        return data.get("metadata", {})
 
-    def __init__(
-        self,
-        aliases: Dict[str, str] = None,
-        exclude: List[str] = None,
-        normalize: bool = False,
-        date_columns: List[str] = None,
-        parsers: List[Parser] = None,
-    ):
-        self.normalize = normalize
-        self.aliases = aliases or {}
-        self.exclude = exclude or []
-        self.date_columns = date_columns or []
-        self.parsers = parsers or []
-        self.errors: List[str] = []
-
-    def __repr__(self):
+    @classmethod
+    def extract_last_updated_date(cls, data: OrderedDict) -> OrderedDict:
         return (
-            f"Transformer: {len(self.aliases)} aliases, {len(self.exclude)} exclusions"
-        )
-
-    def add_parser(
-        self, parser: Parser = None, ruleset: Dict[str, List] = None, name: str = None
-    ):
-        self.parsers.append(parser or Parser.init(ruleset, name=name))
-        return self
-
-    def normalize_keys(self, data: dict) -> dict:
-        return util.apply_transformation(data, sp.normalize, keys=True, values=False)
-
-    def parse_value_dtypes(self, data: dict) -> dict:
-        for parser in self.parsers:
-            data = util.apply_transformation(
-                data, parser.parse, keys=False, values=True
-            )
-        return data
-
-    def transform(self, xml: str, **kwargs) -> dict:
-        parsed = self.xml_to_dict(xml)
-        parsed = self.normalize_keys(parsed)
-        return parsed
-
-    def xml_to_dict(self, xml: str, **kwargs):
-        return xmltodict.parse(xml, **kwargs)
-
-    def handle_date(self, value: str):
-        try:
-            pass
-        except Exception as e:
-            msg = f"Unable to convert value to datetime: {value} -- {e}"
-            self.errors.append(msg)
-            logger.debug(msg)
-            return None
-
-
-class WellboreTransformer(Transformer):
-    def transform(self, xml: str, **kwargs):
-        parsed = super().transform(xml, **kwargs)
-
-    # def extract_collections(self, data: OrderedDict) -> OrderedDict:
-    #     """ Breakout root document into sub-documents """
-    #     pass
-
-    # def apply_metadata_to_collection(self, collection: OrderedDict) -> dict:
-    #     pass
-
-    def extract_metadata(self, root_document: OrderedDict) -> OrderedDict:
-        return root_document.get("metadata", {})
-
-    def extract_last_updated_date(self, root_document: OrderedDict) -> OrderedDict:
-        return (
-            root_document.get("header", {})
+            data.get("header", {})
             .get("dates", {})
-            .get({"last_update"}, {})
+            .get("last_update", {})
             .get("standard")
         )
 
-    def copy_api_to_root(self, root_document: OrderedDict) -> OrderedDict:
+    @classmethod
+    def copy_api_to_root(cls, data: OrderedDict) -> OrderedDict:
         """ Moves a well's identification number (api) to the top level of
             the dictionary."""
 
-        _id = root_document.get("metadata", {}).get("identification")
-        if str(_id).isnumeric():
+        _id = str(cls.extract_metadata(data).get("identification", ""))
+        if _id.isnumeric():
             if len(_id) == 14:
-                root_document["api14"] = _id
-                root_document["api10"] = _id[:10]
+                data["api14"] = _id
+                data["api10"] = _id[:10]
             elif len(_id) == 10:
-                root_document["api10"] = _id
+                data["api10"] = _id
 
-        root_document.move_to_end("api14", last=False)
-        root_document.move_to_end("api10", last=False)
+        data.move_to_end("api10", last=False)
 
-        return root_document
+        return data
+
+    @classmethod
+    def copy_metadata_to_root(cls, data: OrderedDict) -> OrderedDict:
+        meta = cls.extract_metadata(data)
+        for key, value in meta.items():
+            data[key] = value
+            data.move_to_end(key, last=False)
+
+        return data
+
+    @classmethod
+    def copy_last_update_to_root(cls, data: OrderedDict) -> OrderedDict:
+        data["last_update"] = cls.extract_last_updated_date(data)
+        return data
+
+    @classmethod
+    def add_document_hash(cls, data: OrderedDict) -> OrderedDict:
+        data["md5"] = hashlib.md5(str(data).encode()).hexdigest()
+        data.move_to_end("md5", last=False)
+        return data
+
+    @classmethod
+    def transform(cls, data: OrderedDict) -> OrderedDict:
+        parsed = cls.copy_metadata_to_root(data)
+        parsed = cls.copy_last_update_to_root(parsed)
+        parsed = cls.copy_api_to_root(parsed)
+        parsed = cls.add_document_hash(parsed)
+        return parsed
+
+    @classmethod
+    def extract_from_wellset(cls, document: OrderedDict) -> List[OrderedDict]:
+        wellset = document.get("well_set", document)
+        wellbores = wellset.get("wellbore", wellset)
+
+        transformed_wellbores: List[OrderedDict] = []
+        for wb in wellbores:
+            transformed_wellbores.append(WellboreTransformer.transform(wb))
+
+        logger.info(f"Extracted {len(transformed_wellbores)} from document")
+        return transformed_wellbores
 
 
 if __name__ == "__main__":
 
     from collector.endpoint import load_from_config
     from config import get_active_config
+    from collector.xmlparser import XMLParser
 
     logging.basicConfig()
     logger.setLevel(10)
@@ -123,24 +96,19 @@ if __name__ == "__main__":
     endpoints = load_from_config(conf)
     endpoint = endpoints.get("wells")
 
-    t = Transformer(aliases=endpoint.mappings.get("aliases"), exclude=endpoint.exclude)
+    parser = XMLParser.load_from_config(conf.PARSER_CONFIG)
 
-    t.add_parser(
-        ruleset=conf.PARSER_CONFIG["parsers"]["default"]["rules"], name="default"
-    )
+    # parser.add_parser(
+    #     ruleset=conf.PARSER_CONFIG["parsers"]["default"]["rules"], name="default"
+    # )
 
     xml = util.load_xml("test/data/well_header_short.xml")
 
-    parsed = t.xml_to_dict(xml)
-    parsed = t.normalize_keys(parsed)
-    parsed = t.parse_value_dtypes(parsed)
+    document = parser.parse(xml)
+    wellset = document.get("well_set", document)
+    wellbores = wellset.get("wellbore", wellset)
 
-    wellset = parsed.get("well_set")
-    wellbore = wellset.get("wellbore")[0]
-    len(wellbore)
-
-    treatment_summary = wellbore.get("treatment_summary")
-    print(treatment_summary)
-
-    # util.to_json(wellbore[0], "example.json", cls=util.DateTimeEncoder)
+    transformed_wellbores: List[OrderedDict] = []
+    for wb in wellbores:
+        transformed_wellbores.append(WellboreTransformer.transform(wb))
 
---
+++ ihs/collector/xmlparser.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+from typing import Callable, Dict, List, Union  # pylint: disable=unused-import
+from collections import OrderedDict
+import logging
+
+import xmltodict
+import util
+
+from collector.parser import Parser
+
+sp = util.StringProcessor()
+
+logger = logging.getLogger(__name__)
+
+
+class XMLParser(object):
+    """ Transform an XML response into a normalized Python object"""
+
+    def __init__(
+        self,
+        aliases: Dict[str, str] = None,
+        exclude: List[str] = None,
+        normalize: bool = False,
+        parsers: List[Parser] = None,
+    ):
+        self.normalize = normalize
+        self.aliases = aliases or {}
+        self.exclude = exclude or []
+        self.parsers = parsers or []
+        self.errors: List[str] = []
+
+    def __repr__(self):
+        s = "s" if len(self.parsers) > 1 else ""
+        return f"XMLParser: {len(self.parsers)} attached parser{s}"
+
+    def add_parser(
+        self, parser: Parser = None, ruleset: Dict[str, List] = None, name: str = None
+    ):
+        self.parsers.append(parser or Parser.init(ruleset, name=name))
+        return self
+
+    def normalize_keys(self, data: OrderedDict) -> OrderedDict:
+        return util.apply_transformation(data, sp.normalize, keys=True, values=False)
+
+    def parse_value_dtypes(self, data: OrderedDict) -> OrderedDict:
+        for parser in self.parsers:
+            data = util.apply_transformation(
+                data, parser.parse, keys=False, values=True
+            )
+        return data
+
+    def parse(self, xml: str, **kwargs) -> OrderedDict:
+        parsed = self.xml_to_dict(xml, **kwargs)
+        parsed = self.normalize_keys(parsed)
+        parsed = self.parse_value_dtypes(parsed)
+        return parsed
+
+    def xml_to_dict(self, xml: str, **kwargs) -> OrderedDict:
+        return xmltodict.parse(xml, **kwargs)
+
+    @staticmethod
+    def load_from_config(parser_conf: dict):
+        parser_conf = parser_conf.get("parsers", parser_conf)
+        parsers: List[Parser] = []
+        for name, parser_def in parser_conf.items():
+            ruleset = parser_def.get("rules", parser_def)
+            parsers.append(Parser.init(ruleset, name))
+
+        return XMLParser(parsers=parsers)
+
+
+if __name__ == "__main__":
+
+    from collector.endpoint import load_from_config
+    from config import get_active_config
+
+    conf = get_active_config()
+    endpoints = load_from_config(conf)
+    endpoint = endpoints.get("wells")
+
+    parser = XMLParser.load_from_config(conf.PARSER_CONFIG)
