5ae87acb2c0f481fbf8188929a60b07194a042c9
Brock Friedrich <brock@driftwoodenergy.com>
2019-11-17 11:16:32 -0600

    default collector imports

--- ihs/collector/__init__.py
+++ ihs/collector/__init__.py
@@ -1,3 +1,9 @@
-from collector.requestor import Requestor, RestRequestor
-
-# from collector.soap_requestor import SoapRequestor, IHSSoapRequestor
+from collector.xmlparser import XMLParser
+from collector.transformer import WellboreTransformer, ProductionTransformer
+from collector.builder import ExportBuilder, ExportJob, ExportRetriever
+from collector.soap_requestor import SoapRequestor
+from collector.export_parameter import ExportParameter
+from collector.endpoint import Endpoint
+from collector.task import Task, OptionMatrix
+from collector.identity_list import WellList, ProducingEntityList
+from collector.collector import Collector
