e6fdf8f0371e14a9ae13aaf94a2eaa378c417402
Brock Friedrich <brock@driftwoodenergy.com>
2019-11-18 10:43:57 -0600

    sqs policy update

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -60,7 +60,9 @@ data "aws_iam_policy_document" "allow_ecs_access_to_sqs" {
     ]
 
     resources = [
-      aws_sqs_queue.celery.arn
+      aws_sqs_queue.default.arn,
+      aws_sqs_queue.collections.arn,
+      aws_sqs_queue.submissions.arn,
     ]
 
     condition {
