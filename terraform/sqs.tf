6d8d8c078b8ce2941fa3af640e7727887a411806
Brock Friedrich <brocklfriedrich@gmail.com>
2019-11-27 09:06:37 -0600

    added more queues

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,49 +1,100 @@
 resource "aws_sqs_queue" "default" {
   name                       = "${var.service_name}-default"
-  delay_seconds              = 30
+  delay_seconds              = 30   # hide message for 30 seconds before making available to consumers
   message_retention_seconds  = 3600 # 1 hour
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 360 # 5 mintues
-
-  tags = local.tags
+  tags                       = local.tags
 }
 
-resource "aws_sqs_queue_policy" "sqs-default" {
+resource "aws_sqs_queue_policy" "default" {
   queue_url = aws_sqs_queue.default.id
   policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
 }
 
-resource "aws_sqs_queue" "collections" {
-  name                       = "${var.service_name}-collections"
-  delay_seconds              = 30
-  message_retention_seconds  = 3600 # 1 hour
+resource "aws_sqs_queue" "collections_h" {
+  name                       = "${var.service_name}-collections-h"
+  delay_seconds              = 30       # hide message for 30 seconds before making available to consumers
+  message_retention_seconds  = 3600 * 6 # 6 hours
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 360 # 5 mintues
-
-  tags = local.tags
+  tags                       = local.tags
 }
 
-resource "aws_sqs_queue_policy" "collections" {
-  queue_url = aws_sqs_queue.collections.id
+resource "aws_sqs_queue_policy" "collections_h" {
+  queue_url = aws_sqs_queue.collections_h.id
   policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
 }
 
-resource "aws_sqs_queue" "submissions" {
-  name                       = "${var.service_name}-submissions"
+resource "aws_sqs_queue" "submissions_h" {
+  name                       = "${var.service_name}-submissions-h"
   delay_seconds              = 0
-  message_retention_seconds  = 3600 # 1 hour
+  message_retention_seconds  = 3600 * 6 # 6 hours
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 360 # 5 mintues
-
-  tags = local.tags
+  tags                       = local.tags
 }
 
-resource "aws_sqs_queue_policy" "submissions" {
-  queue_url = aws_sqs_queue.submissions.id
+resource "aws_sqs_queue_policy" "submissions_h" {
+  queue_url = aws_sqs_queue.submissions_h.id
   policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
 }
 
+resource "aws_sqs_queue" "deletions_h" {
+  name                       = "${var.service_name}-deletions-h"
+  delay_seconds              = 0
+  message_retention_seconds  = 3600 * 6 # 6 hours
+  receive_wait_time_seconds  = 0
+  visibility_timeout_seconds = 360 # 5 mintues
+  tags                       = local.tags
+}
 
+resource "aws_sqs_queue_policy" "deletions_h" {
+  queue_url = aws_sqs_queue.deletions_h.id
+  policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
+}
+
+resource "aws_sqs_queue" "collections_v" {
+  name                       = "${var.service_name}-collections-v"
+  delay_seconds              = 30       # hide message for 30 seconds before making available to consumers
+  message_retention_seconds  = 3600 * 6 # 6 hours
+  receive_wait_time_seconds  = 0
+  visibility_timeout_seconds = 360 # 5 mintues
+  tags                       = local.tags
+}
+
+resource "aws_sqs_queue_policy" "collections_v" {
+  queue_url = aws_sqs_queue.collections_v.id
+  policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
+}
+
+resource "aws_sqs_queue" "submissions_v" {
+  name                       = "${var.service_name}-submissions-v"
+  delay_seconds              = 0
+  message_retention_seconds  = 3600 * 6 # 6 hours
+  receive_wait_time_seconds  = 0
+  visibility_timeout_seconds = 360 # 5 mintues
+  tags                       = local.tags
+}
+
+resource "aws_sqs_queue_policy" "submissions_v" {
+  queue_url = aws_sqs_queue.submissions_v.id
+  policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
+}
+
+resource "aws_sqs_queue" "deletions_v" {
+  name                       = "${var.service_name}-deletions-v"
+  delay_seconds              = 0
+  message_retention_seconds  = 3600 * 6 # 6 hours
+  receive_wait_time_seconds  = 0
+  visibility_timeout_seconds = 360 # 5 mintues
+  tags                       = local.tags
+}
+
+resource "aws_sqs_queue_policy" "deletions_v" {
+  queue_url = aws_sqs_queue.deletions_v.id
+  policy    = data.aws_iam_policy_document.allow_ecs_access_to_sqs.json
+}
 
 data "aws_iam_policy_document" "allow_ecs_access_to_sqs" {
   statement {
@@ -61,8 +112,12 @@ data "aws_iam_policy_document" "allow_ecs_access_to_sqs" {
 
     resources = [
       aws_sqs_queue.default.arn,
-      aws_sqs_queue.collections.arn,
-      aws_sqs_queue.submissions.arn,
+      aws_sqs_queue.collections_h.arn,
+      aws_sqs_queue.submissions_h.arn,
+      aws_sqs_queue.deletions_h.arn,
+      aws_sqs_queue.collections_v.arn,
+      aws_sqs_queue.submissions_v.arn,
+      aws_sqs_queue.deletions_v.arn,
     ]
 
     condition {
@@ -70,11 +125,5 @@ data "aws_iam_policy_document" "allow_ecs_access_to_sqs" {
       variable = "ecs:service"
       values   = [aws_ecs_service.ihs.id]
     }
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "aws:Referer"
-    #   values   = [data.aws_caller_identity.current.account_id]
-    # }
-
   }
 }
