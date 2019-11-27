6946800d72e9688f7a13139b58f9a297ebffc02f
Brock Friedrich <brocklfriedrich@gmail.com>
2019-11-27 09:07:17 -0600

    router comments

--- ihs/celery_queue/routers.py
+++ ihs/celery_queue/routers.py
@@ -5,13 +5,25 @@ conf = get_active_config()
 
 
 def hole_direction_router(name, args, kwargs, options, task=None, **kw):
-    """ Route tasks based on name and the hole_direction (H or V) given as the first
-        arg in the task signature."""
+    """ Route tasks based on name and the hole_direction (H or V) given as the first arg in the task signature.
+
+    Results in 6 routable queues:
+
+        Horizontals:
+        - ihs-submissions-h
+        - ihs-collections-h
+        - ihs-deletions-h
+
+        Verticals:
+        - ihs-submissions-v
+        - ihs-collections-v
+        - ihs-deletions-v
+    """
     is_horizontal = args[0] == HoleDirection.H.name
 
     # base queue definitions
     queue_map = {
-        "celery_queue.tasks.submit_job": f"{project}-submissions",
+        "celery_queue.tasks.submit_job": f"{project}-deletions",
         "celery_queue.tasks.collect_job_result": f"{project}-collections",
         "celery_queue.tasks.delete_job": f"{project}-deletions",
     }
