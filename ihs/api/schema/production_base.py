ab1bab0d1ded9816cad553e38a2cfe631b7fd523
Brock Friedrich <brocklfriedrich@gmail.com>
2019-12-02 07:51:29 -0600

    base production validation schema

---
+++ ihs/api/schema/production_base.py
@@ -0,0 +1,29 @@
+# pylint: disable=unused-argument
+
+from typing import Dict
+import functools
+from datetime import timezone
+
+from marshmallow import Schema, fields, pre_dump
+
+from util import query_dict
+from api.schema.validators import length_is_14, length_is_10
+
+
+class ProductionBaseSchema(Schema):
+    class Meta:
+        ordered = True
+
+    api10 = fields.Str(required=True, validate=length_is_10)
+    entity = fields.Str(required=True)
+    last_update_at = fields.AwareDateTime(default_timezone=timezone.utc)
+    hole_direction = fields.Str()
+
+    @pre_dump
+    def transform(self, data, **kwargs) -> Dict:
+        output: Dict = {}
+
+        if hasattr(data, "production_header"):
+            output = data.production_header
+
+        return output
