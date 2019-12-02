21a2dfb6d934184f303335fab1977790697329a7
Brock Friedrich <brocklfriedrich@gmail.com>
2019-12-02 07:54:05 -0600

    added api length validators

--- ihs/api/schema/validators.py
+++ ihs/api/schema/validators.py
@@ -5,3 +5,14 @@ def not_blank(data):
     if not data:
         raise ValidationError("Data not provided.")
 
+
+def length_is_14(data):
+    target = 14
+    if len(data) != target:
+        raise ValidationError(f"Incorrect length: {len(data)} (should be {target})")
+
+
+def length_is_10(data):
+    target = 10
+    if len(data) != target:
+        raise ValidationError(f"Incorrect length: {len(data)} (should be {target})")
