c5b2b4aee691bb47626687fdffb54227323be6d9
Brock Friedrich <brocklfriedrich@gmail.com>
2019-12-05 17:40:03 -0600

    separated cloudwatch log groups

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,5 +1,35 @@
-resource "aws_cloudwatch_log_group" "loggroup" {
-  name              = "/ecs/${var.service_name}-deo"
+resource "aws_cloudwatch_log_group" "ihs_worker_default" {
+  name              = "/ecs/ihs-worker-default"
+  retention_in_days = 3
+  tags              = "${local.tags}"
+}
+
+resource "aws_cloudwatch_log_group" "ihs_worker_submitter" {
+  name              = "/ecs/ihs-worker-submitter"
+  retention_in_days = 3
+  tags              = "${local.tags}"
+}
+
+resource "aws_cloudwatch_log_group" "ihs_worker_deleter" {
+  name              = "/ecs/ihs-worker-deleter"
+  retention_in_days = 3
+  tags              = "${local.tags}"
+}
+
+resource "aws_cloudwatch_log_group" "ihs_worker_collector" {
+  name              = "/ecs/ihs-worker-collector"
+  retention_in_days = 3
+  tags              = "${local.tags}"
+}
+
+resource "aws_cloudwatch_log_group" "ihs_cron" {
+  name              = "/ecs/ihs-cron"
+  retention_in_days = 3
+  tags              = "${local.tags}"
+}
+
+resource "aws_cloudwatch_log_group" "ihs_web" {
+  name              = "/ecs/ihs-web"
   retention_in_days = 3
   tags              = "${local.tags}"
 }
