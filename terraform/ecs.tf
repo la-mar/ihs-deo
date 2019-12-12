2a80d182239656bc814c737d235711deec8b549d
Brock Friedrich <brocklfriedrich@gmail.com>
2019-12-12 09:36:06 -0600

    added permissions for Chamber

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -193,13 +193,13 @@ resource "aws_ecs_service" "ihs_cron" {
 }
 
 # Define Task Role
-resource "aws_iam_role" "ecs_task_role" {
+resource "aws_iam_role" "task_role" {
   name               = "${var.service_name}-task-role"
-  assume_role_policy = data.aws_iam_policy_document.task_policy.json
+  assume_role_policy = data.aws_iam_policy_document.task_sts_policy.json
   tags               = local.tags
 }
 
-data "aws_iam_policy_document" "task_policy" {
+data "aws_iam_policy_document" "task_sts_policy" {
   statement {
     sid    = ""
     effect = "Allow"
@@ -214,14 +214,14 @@ data "aws_iam_policy_document" "task_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_ecs_service_policy_to_task_role" {
-  role       = aws_iam_role.ecs_task_role.name
+  role       = aws_iam_role.task_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-
 # Allow task role to talk to SQS
-data "aws_iam_policy_document" "allow_task_access_to_sqs" {
+data "aws_iam_policy_document" "task_policy" {
   statement {
+    sid = "task_access_sqs"
     actions = [
       "sqs:*",
     ]
@@ -236,17 +236,40 @@ data "aws_iam_policy_document" "allow_task_access_to_sqs" {
       aws_sqs_queue.deletions_v.arn,
     ]
   }
+
+  statement {
+    sid = "task_access_secrets"
+    actions = [
+      "ssm.GetParameters"
+    ]
+    resources = [
+      "arn:aws:ssm:*:*:parameter/${var.service_name}/*"
+    ]
+  }
+
+  statement {
+    sid = "task_access_kms"
+    actions = [
+      "kms:ListKeys",
+      "kms:ListAliases",
+      "kms:Describe*",
+      "kms:Decrypt"
+    ]
+    resources = [
+      "parameter_store_key"
+    ]
+  }
 }
 
-resource "aws_iam_policy" "allow_task_access_to_sqs" {
+resource "aws_iam_policy" "task_policy" {
   name        = "${var.service_name}-task-access-sqs"
   path        = "/"
   description = "Allow ${var.service_name} tasks running in ECS to interface with SQS queues"
 
-  policy = data.aws_iam_policy_document.allow_task_access_to_sqs.json
+  policy = data.aws_iam_policy_document.task_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "attach_sqs_policy_to_task_role" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.allow_task_access_to_sqs.arn
+resource "aws_iam_role_policy_attachment" "attach_task_policy_to_task_role" {
+  role       = aws_iam_role.task_role.name
+  policy_arn = aws_iam_policy.task_policy.arn
 }
