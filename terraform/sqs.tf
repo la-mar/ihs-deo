61869020425ef87904b653237692654723942950
Brock Friedrich <brocklfriedrich@gmail.com>
2019-12-12 11:44:38 -0600

    arn is accessed through id property

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -26,12 +26,12 @@ data "aws_iam_policy_document" "allow_ecs_task_access_to_sqs" {
       test     = "ArnEquals"
       variable = "aws:SourceArn"
       values = [
-        aws_ecs_service.ihs_web.arn,
-        aws_ecs_service.ihs_worker_submitter.arn,
-        aws_ecs_service.ihs_worker_deleter.arn,
-        aws_ecs_service.ihs_worker_collector.arn,
-        aws_ecs_service.ihs_worker_default.arn,
-        aws_ecs_service.ihs_cron.arn,
+        aws_ecs_service.ihs_web.id,
+        aws_ecs_service.ihs_worker_submitter.id,
+        aws_ecs_service.ihs_worker_deleter.id,
+        aws_ecs_service.ihs_worker_collector.id,
+        aws_ecs_service.ihs_worker_default.id,
+        aws_ecs_service.ihs_cron.id,
       ]
     }
     resources = [
