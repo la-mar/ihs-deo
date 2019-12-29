ff387410dbd78eb392009fc069b5bad0c438a122
Brock Friedrich <brocklfriedrich@gmail.com>
2019-12-29 10:38:23 -0600

    increased sqs message retention

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -64,7 +64,7 @@ resource "aws_sqs_queue_policy" "default" {
 resource "aws_sqs_queue" "collections_h" {
   name                       = "${var.service_name}-collections-h"
   delay_seconds              = 30        # hide message for 30 seconds before making available to consumers
-  message_retention_seconds  = 3600 * 24 # 24 hours
+  message_retention_seconds  = 3600 * 48 # 48 hours
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 600 # 10 min
   tags                       = merge(local.tags, { class = local.hz })
@@ -74,7 +74,7 @@ resource "aws_sqs_queue" "collections_h" {
 resource "aws_sqs_queue" "submissions_h" {
   name                       = "${var.service_name}-submissions-h"
   delay_seconds              = 0
-  message_retention_seconds  = 3600 * 24 # 24 hours
+  message_retention_seconds  = 3600 * 48 # 48 hours
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 600 # 10 min
   tags                       = merge(local.tags, { class = local.hz })
@@ -85,7 +85,7 @@ resource "aws_sqs_queue" "submissions_h" {
 resource "aws_sqs_queue" "deletions_h" {
   name                       = "${var.service_name}-deletions-h"
   delay_seconds              = 0
-  message_retention_seconds  = 3600 * 24 # 24 hours
+  message_retention_seconds  = 3600 * 48 # 48 hours
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 600 # 10 min
   tags                       = merge(local.tags, { class = local.hz })
@@ -95,8 +95,8 @@ resource "aws_sqs_queue" "deletions_h" {
 
 resource "aws_sqs_queue" "collections_v" {
   name                       = "${var.service_name}-collections-v"
-  delay_seconds              = 30        # hide message for 30 seconds before making available to consumers
-  message_retention_seconds  = 3600 * 72 # 72 hours
+  delay_seconds              = 30         # hide message for 30 seconds before making available to consumers
+  message_retention_seconds  = 3600 * 144 # 6 days
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 600 # 10 min
   tags                       = merge(local.tags, { class = local.vt })
@@ -105,7 +105,7 @@ resource "aws_sqs_queue" "collections_v" {
 resource "aws_sqs_queue" "submissions_v" {
   name                       = "${var.service_name}-submissions-v"
   delay_seconds              = 0
-  message_retention_seconds  = 3600 * 72 # 72 hours
+  message_retention_seconds  = 3600 * 144 # 6 days
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 600 # 10 min
   tags                       = merge(local.tags, { class = local.vt })
@@ -114,7 +114,7 @@ resource "aws_sqs_queue" "submissions_v" {
 resource "aws_sqs_queue" "deletions_v" {
   name                       = "${var.service_name}-deletions-v"
   delay_seconds              = 0
-  message_retention_seconds  = 3600 * 72 # 72 hours
+  message_retention_seconds  = 3600 * 144 # 6 days
   receive_wait_time_seconds  = 0
   visibility_timeout_seconds = 600 # 10 min
   tags                       = merge(local.tags, { class = local.vt })
