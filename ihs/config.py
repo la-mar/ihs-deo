cf9b480617605d6de936eacc08df5ea4142530fc
Brock Friedrich <brocklfriedrich@gmail.com>
2020-04-30 13:20:52 -0500

    added CELERYBEAT_MAX_LOOP_INTERVAL option

--- ihs/config.py
+++ ihs/config.py
@@ -226,8 +226,7 @@ class BaseConfig:
     REDBEAT_REDIS_URL = os.getenv("IHS_CRON_URL")
     REDBEAT_KEY_PREFIX = f"{project}:"
     CELERYBEAT_LOAD_ENDPOINTS: bool = to_bool(os.getenv("CELERYBEAT_LOAD_ENDPOINTS", True))
-
-
+    CELERYBEAT_MAX_LOOP_INTERVAL: int = to_int(os.getenv("CELERYBEAT_MAX_LOOP_INTERVAL", 30))
 
     """ API """
     API_CLIENT_TYPE = os.getenv("IHS_CLIENT_TYPE", "legacy")
